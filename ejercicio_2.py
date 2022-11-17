6dbf90fac997c1d7d2a3bd9357369e3e8e9d3c90
Katherine712 <118409863+Katherine712@users.noreply.github.com>
2022-11-17 15:29:10 -0300

    Update ejercicio_2.py

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,1 +1,17 @@
+#Ejercicio 2
 
+lista = int(input("Ingrese la cantidad de numeros \n"))
+cont = 0
+for i in range(2, lista + 1):
+    primos = True
+    for j in range(2,i):
+        if i == j:
+           break
+        elif i%j == 0:
+           primos = False
+        else:
+           continue
+    if primos == True:
+        print(' ',i, end='')
+        cont += 1
+print('\nHay %u números primos.' % cont )
